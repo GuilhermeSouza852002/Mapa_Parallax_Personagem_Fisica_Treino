fea4cf4293108cdb6e25eeb4e3b1c71eebefa4ee
Guilherme O. Souza <ShadowDragonRub@gmail.com>
2023-02-23 20:31:11 -0300

    V0.3

--- level.py
+++ level.py
@@ -1,19 +1,27 @@
 import pygame
 from tiles import Tile
+from settings import tile_size
 
 class Level:
     def __init__(self,level_data,surface):
+        # level setup
         self.display_surface = surface
         self.setup_level(level_data)
         
+        self.world_shift = 0
      #O conjunto de X ganha forma no mapa
      
     #percorre as linhas do mapa e as númera 
     def setup_level(self,layout):   
          self.tiles = pygame.sprite.Group()
-         for row_index,row in enumerate(layout): 
-             print(row_index)
-             print(row)
+         for row_index,row in enumerate(layout):
+             for col_index,cell in enumerate(row):
+                 if cell == 'X':
+                     x = col_index * tile_size
+                     y = row_index * tile_size
+                     tile = Tile((x,y),tile_size) 
+                     self.tiles.add(tile)
         
     def run(self):
+        self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
