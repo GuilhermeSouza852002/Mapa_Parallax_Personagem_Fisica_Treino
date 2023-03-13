9ac874f0ad7793515e8c2e9112d5831bb8621be9
Guilherme O. Souza <ShadowDragonRub@gmail.com>
2023-03-13 01:14:13 -0300

    V1.5

--- tiles.py
+++ tiles.py
@@ -5,5 +5,7 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
         self.image = pygame.Surface((tile_size,tile_size))
-        self.image.fill('#65220d')
+        #self.image.fill('#65220d')
+        texture = pygame.image.load('mapa\bloco.png')    #mapa fundo
+        self.image.blit(texture, (0, 0))
         self.rect = self.image.get_rect(topleft = pos)
